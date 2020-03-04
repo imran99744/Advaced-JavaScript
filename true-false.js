a6f6a22ca4825e344a6ff11a7e5e3d497e2cd4c4
anjuara1978 <anjuarabegum1978@gmail.com>
2020-03-04 17:57:17 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,22 @@
+// falsy:
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// Truthy
+// "0", " "
+
+
+
+const name = "";
+
+if (name){
+    console.log("condition is true");
+
+}
+
+else{
+    console.log("condition is fales")
+}
